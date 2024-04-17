f83625b9adb6e8a57368cfbaee9ad945b9cb4185
Anwar5533 <anwarkornipalli@gmail.com>
2024-04-17 09:30:40 +0530

    New changes

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -1,10 +1,36 @@
+import { useState } from "react";
 import { assets } from "../../assets/assets";
 
 export default function FoodItem({ id, name, price, description, image }) {
+  const [itemCount, setItemCount] = useState(0);
   return (
-    <div className="w-100% delay-0.3s mb-16 animate-fadeIn rounded-lg shadow-md shadow-slate-200 transition">
+    <div className="w-100% delay-0.3s mb-16 animate-fadeIn rounded-lg shadow-md shadow-slate-200 transition hover:shadow-slate-500 ">
       <div>
         <img src={image} alt="" className="w-100% rounded-t-xl" />
+        {!itemCount ? (
+          <img
+            onClick={() => setItemCount((prev) => prev + 1)}
+            src={assets.add_icon_white}
+            alt=""
+            className="cursor-pointer"
+          />
+        ) : (
+          <div className="">
+            <img
+              onClick={() => setItemCount((prev) => prev - 1)}
+              src={assets.remove_icon_red}
+              alt=""
+              className="cursor-pointer"
+            />
+            <p>{itemCount}</p>
+            <img
+              onClick={() => setItemCount((prev) => prev + 1)}
+              src={assets.add_icon_green}
+              alt=""
+              className="cursor-pointer"
+            />
+          </div>
+        )}
       </div>
       <div className=" p-[20px] ">
         <div className=" mb-[10px] flex items-center justify-between ">
