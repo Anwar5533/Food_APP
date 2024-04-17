fdc7d4013756e0ed4f8f5bf5f66ad71be138bed3
Anwar5533 <anwarkornipalli@gmail.com>
2024-04-17 09:53:07 +0530

    Filtered list

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -3,24 +3,32 @@ import { StoreContext } from "../../Context/StoreContext";
 import FoodItem from "../FoodItem/FoodItem";
 
 export default function FoodDisplay({ category }) {
+  
   const { food_list } = useContext(StoreContext);
 
   return (
+
     <div id="food-display" className="mt-[30px]">
       <h2 className="mb-[10px] text-[40px] text-base md:text-[2vw]">
         Top dishes near you
       </h2>
       <div className="mt-[30px] grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
-        {food_list.map((item) => (
-          <FoodItem
-            key={item._id}
-            id={item._id}
-            name={item.name}
-            description={item.description}
-            price={item.price}
-            image={item.image}
-          />
-        ))}
+        {food_list.map((item) => {
+          {console.log(category, item.category)}
+          if (category === "All" || category === item.category) {
+            return (
+              <FoodItem
+                key={item._id}
+                id={item._id}
+                name={item.name}
+                description={item.description}
+                price={item.price}
+                image={item.image}
+              />
+            );
+          }
+          
+})}
       </div>
     </div>
   );
